fea6853b32cb53789f9cf8d82fa55d02fbe77e9e
greywind7 <ritik.aggr@gmail.com>
2021-06-10 12:04:54 +0530

    Hello world!

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/kd6-3dot7/Documents/Rust/hello_world/target/debug/hello_world: /home/kd6-3dot7/Documents/Rust/hello_world/src/main.rs
---
+++ hello_world/target/rls/debug/deps/hello_world-617520a0ef390165.d
@@ -0,0 +1,5 @@
+/home/kd6-3dot7/Documents/Rust/hello_world/target/rls/debug/deps/hello_world-617520a0ef390165.rmeta: src/main.rs
+
+/home/kd6-3dot7/Documents/Rust/hello_world/target/rls/debug/deps/hello_world-617520a0ef390165.d: src/main.rs
+
+src/main.rs:
