ad6d84ce60b0e384a21df024e2867f2d90be4e03
greywind7 <ritik.aggr@gmail.com>
2021-06-18 14:43:42 +0530

    Fixed some bugs in generics

--- generics.rs
+++ generics.rs
@@ -13,16 +13,19 @@ fn make_pair<T:PartialOrd> (fir:T,sec:T)-> Pair<T>{
 }
 
 // Ord/PartialOrd must be specified for comparisons
+// Dunno how this is working, but remember this as is
 impl<T:PartialOrd> Pair<T>{
-    fn max (self)-> T{
+    fn max (&self)-> &T{
         if self.fir > self.sec{
-            return self.fir;
+            return &self.fir;
         }
-        return self.sec;
+        return &self.sec;
     }
 }
 
 fn main(){
     let paio = make_pair(5.99,10.01);
     println!("The bigger number is {}",paio.max());
+    let x:f64 = *paio.max(); 
+    println!("Beebeeboo ooo {} {} {}",paio.fir,paio.sec, x);
 }
